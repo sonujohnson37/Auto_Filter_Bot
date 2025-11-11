e87e95781dff067b77113d3c386977e600fe1f21
Snowtrip <sonujohnsonon2005@gmail.com>
2025-11-11 10:57:33 +0530

    Some normal changes

--- Script.py
+++ Script.py
@@ -1,5 +1,5 @@
 class script(object):
-    START_TXT = """<b><u>🚩 ᴊᴀɪ ꜱʜʀɪ ʀᴀᴍ 🚩</u></b>
+    START_TXT = """<b><u> Hello, Amigo I'm Anya</u></b>
 
 <b>ʜᴇʏ {}, {}</b>
 
@@ -7,7 +7,7 @@ class script(object):
 """
 
 
-    GSTART_TXT = """<b>🚩 ᴊᴀɪ ꜱʜʀɪ ʀᴀᴍ 🚩</b>
+    GSTART_TXT = """<b> Hello Amigo's I'm Anya </b>
 
 <b>ʜᴇʏ {},</b>
 
